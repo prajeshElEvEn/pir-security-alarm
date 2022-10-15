6777bb4cfcc4a5346e33c9d9d497552ecdb01571
Vedasto <prajesh.eleven118@gmail.com>
2022-10-16 03:59:53 +0530

    updated script

--- main.cpp
+++ main.cpp
@@ -1,2 +1,20 @@
 const int pirSensor = 7;
 const int ledPin = 6;
+const int buzzerPin = 5;
+const int buttonPin = 12;
+
+bool buzzerMode = false;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    pinMode(buzzerMode, OUTPUT);
+    pinMode(buttonPin, INPUT);
+}
+
+void loop()
+{
+    if (pirSensor)
+    {
+    }
+}
